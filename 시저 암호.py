c04bec4cced623f82cf4d87f9ef2d6229972dd47
KyungRyeolBaek <40240766+KyungRyeolBaek@users.noreply.github.com>
2020-12-06 03:38:34 +0900

    Update 시저 암호.py

--- 시저 암호.py
+++ 시저 암호.py
@@ -14,6 +14,9 @@ def solution(s, n):
             answer += i
     return answer
 
+# string.ascii_lowercase [a-z] 문자열
+# string.ascii_uppercase [A-Z] 문자열
+
 # 문제 설명
 # 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 AB는 1만큼 밀면 BC가 되고, 3만큼 밀면 DE가 됩니다. z는 1만큼 밀면 a가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
 
