fbfab2f2e6b29db2c9ea6567664576ceb5ae0443
KyungRyeolBaek <40240766+KyungRyeolBaek@users.noreply.github.com>
2020-12-05 15:55:02 +0900

    Update 두 개 뽑아서 더하기.py

--- 두 개 뽑아서 더하기.py
+++ 두 개 뽑아서 더하기.py
@@ -1,1 +1,5 @@
-
+def solution(numbers):
+    answer = [a + b for a in sorted(numbers) for b in sorted(numbers)]
+    for a in [2*a for a in sorted(numbers)]:
+        answer.remove(a)
+    return list(set(answer))
