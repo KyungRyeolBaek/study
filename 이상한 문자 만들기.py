42c2ecb4feeef778fa3df78d4695889a65b5908b
KyungRyeolBaek <40240766+KyungRyeolBaek@users.noreply.github.com>
2020-12-08 00:28:46 +0900

    Update 이상한 문자 만들기.py

--- 이상한 문자 만들기.py
+++ 이상한 문자 만들기.py
@@ -1,4 +1,32 @@
 def solution(s):
-    return ''.join([list(s)[i].upper() if i%2==0 else list(s)[i].lower() for i in range(len(s))])
-    
-    
+    answer = ''
+    counter = 0
+    for i in s:
+        if i.isspace():
+            answer += ' '
+            counter = 0
+        elif counter%2 == 0:
+            answer += i.upper()
+            counter += 1
+        else:
+            answer += i.lower()
+            counter += 1
+    return answer
+
+# a.isspace() a가 공백인가
+
+
+
+
+
+# 문제 설명
+# 문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다. 각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+# 제한 사항
+# 문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+# 첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+# 입출력 예
+# s	return
+# try hello world	TrY HeLlO WoRlD
+# 입출력 예 설명
+# try hello world는 세 단어 try, hello, world로 구성되어 있습니다. 각 단어의 짝수번째 문자를 대문자로, 홀수번째 문자를 소문자로 바꾸면 TrY, HeLlO, WoRlD입니다. 따라서 TrY HeLlO WoRlD 를 리턴합니다.
