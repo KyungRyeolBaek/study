fb6e24fb5d44ff18618a82ce8b6ac7aaafd2022e
forev <foreversanta@naver.com>
2021-04-26 00:20:25 +0900

    2021-04-25
    coin trading

--- coin/upbit/pyupbit-autotrade-main/bitcoinAutoTradeWithSlack_1day.py
+++ coin/upbit/pyupbit-autotrade-main/bitcoinAutoTradeWithSlack_1day.py
@@ -6,7 +6,7 @@ import numpy as np
 
 access = "naahaVSh29BJPu3pfBZPEWSyjsGwytkYiexmAuHR"
 secret = "e0fJfTu1BjPnn35Qc2pAOheSn7xCOXIMxG0Dzluj"
-myToken = "xoxb-1995417427669-2004663172180-cd7eYVuTCWvODSYzgDNkHoOe"
+myToken = "xoxb-1995417427669-2004663172180-uc42mCLSDLoYUsfSJk2zOBYD"
 
 slack_channel = "#trade"
 coin = "EMC2"
@@ -70,34 +70,34 @@ print(coin +" autotrade start")
 # 시작 메세지 슬랙 전송
 post_message(myToken, slack_channel, coin + " autotrade start")
 
-# while True:
-#     try:
-#         now = datetime.datetime.now()
-#         start_time = get_start_time(coin_name)
-#         end_time = start_time + datetime.timedelta(days=1)
-#         print(now)
-#         if start_time < now < end_time - datetime.timedelta(seconds=10):
-#             highk = []
-#             for k in np.arange(0.1, 1.0, 0.1):
-#                 ror = get_ror(k)
-#                 highk.append([ror, k])
-#             time.sleep(1)
-#             target_price = get_target_price(coin_name, max(highk)[1])
-#             ma15 = get_ma15(coin_name)
-#             current_price = get_current_price(coin_name)
-#             if target_price < current_price and ma15 < current_price:
-#                 krw = get_balance("KRW")
-#                 if krw > 5000:
-#                     buy_result = upbit.buy_market_order(coin_name, krw*0.9995)
-#                     post_message(myToken, slack_channel, coin+" buy : " +str(buy_result))
-#
-#         else:
-#             btc = get_balance(coin)
-#             if btc > 0.00008:
-#                 sell_result = upbit.sell_market_order(coin_name, btc*0.9995)
-#                 post_message(myToken, slack_channel, coin+" sell : " +str(sell_result))
-#         time.sleep(1)
-#     except Exception as e:
-#         print(e)
-#         post_message(myToken, slack_channel, e)
-#         time.sleep(1)
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time(coin_name)
+        end_time = start_time + datetime.timedelta(days=1)
+        print(now)
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            highk = []
+            for k in np.arange(0.1, 1.0, 0.1):
+                ror = get_ror(k)
+                highk.append([ror, k])
+            time.sleep(1)
+            target_price = get_target_price(coin_name, max(highk)[1])
+            ma15 = get_ma15(coin_name)
+            current_price = get_current_price(coin_name)
+            if target_price < current_price and ma15 < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order(coin_name, krw*0.9995)
+                    post_message(myToken, slack_channel, coin+" buy : " +str(buy_result))
+
+        else:
+            btc = get_balance(coin)
+            if btc > 0.00008:
+                sell_result = upbit.sell_market_order(coin_name, btc*0.9995)
+                post_message(myToken, slack_channel, coin+" sell : " +str(sell_result))
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        post_message(myToken, slack_channel, e)
+        time.sleep(1)
