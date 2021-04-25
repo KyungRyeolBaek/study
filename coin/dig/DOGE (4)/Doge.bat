fa78202494794f59578ef17b590e448fe53f68d1
forev <foreversanta@naver.com>
2021-04-25 13:35:17 +0900

    2021-04-25
    coin trading

---
+++ coin/dig/DOGE (4)/Doge.bat
@@ -0,0 +1,6 @@
+REM IMPORTANT: Replace DOGECOIN with your own DOGE wallet address after DOGE: And before the . (Worker1 is the name of the miner)
+PhoenixMiner.exe -pool ethash.unmineable.com:3333 -wal DOGE:D6SZ4it1eCcmDGqXhLHeionHuqxgb6H7EH.home#pbww-qkvb -pass x
+
+
+pause
+
