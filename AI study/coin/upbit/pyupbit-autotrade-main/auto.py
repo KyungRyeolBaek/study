81c80a65f0a5de758a03ba56122a0e680e70dde3
KyungRyeol <foreversanta@naver.com>
2021-10-21 15:23:01 +0900

    coin

--- AI study/coin/upbit/pyupbit-autotrade-main/auto.py
+++ AI study/coin/upbit/pyupbit-autotrade-main/auto.py
@@ -34,41 +34,60 @@ upbit = pyupbit.Upbit(access, secret)
 tickers = pyupbit.get_tickers('KRW')
 
 # 잔고가 있을 때 오르는 종목 사고, 내리면 팔고
-# 30초 간격으로 확인
+# 등락폭 1% 기준으로 둠. 
+# 1분 간격으로 확인
+
+# 수익률 저장
+def save_earning_rate(df):
+    earning_rate = df.sort_values(by = '수익률', axis = 0, ascending=False)
+    print(earning_rate)
+    # 수익률 csv 파일로 저장
+    earning_rate.to_csv('./earning_rate.csv')
+
 coin_dic = dict()
 earning_rate = pd.DataFrame(data = [[0, 0, 0, 0]]*len(tickers), columns=['현재 구매액', '수익', '총 구매액', '수익률'], index=tickers)
 
+# 코인 판매.
 for item in tickers:
-  coin_dic[item] = pyupbit.get_current_price(item)
+    coin_dic[item] = pyupbit.get_current_price(item)
+    coin = upbit.get_balance(item)
+    if coin:
+        upbit.sell_market_order(item, coin)
+        time.sleep(0.21)
+    time.sleep(0.03)
 
 while True:
   buy_list = []
-  print(earning_rate.sort_values(by = '수익률', axis = 0, ascending=False))
   time.sleep(60 - datetime.datetime.now().second)
   for item in tickers:
     current_price = pyupbit.get_current_price(item)
-    margin = current_price - coin_dic[item]
-    if margin > 0:
+    time.sleep(0.03)
+    margin = (current_price - coin_dic[item]) / current_price
+    if margin > 0.01:
       coin_dic[item] = current_price
       buy_list.append(item)
-    elif margin < 0:
+    elif margin < -0.01:
       # 코인 판매.
       current_moeny = upbit.get_balance('KRW')
       coin = upbit.get_balance(item)
       if coin:
         upbit.sell_market_order(item, coin)
         tmp_money = upbit.get_balance('KRW')
-        earning_rate['수익'][item] = tmp_moeny - current_moeny - earning_rate['현재 구매액'][item]
+        earning_rate['수익'][item] = tmp_money - current_moeny - earning_rate['현재 구매액'][item]
         earning_rate['현재 구매액'][item] = 0
         earning_rate['수익률'] = round(earning_rate['수익'] / earning_rate['총 구매액'] * 100, 2)
+        save_earning_rate(earning_rate)
         # time.sleep(0.21)
-        
+      coin_dic[item] = current_price
+    else:
+      coin_dic[item] = current_price
 
   # 코인 구매
-  if upbit.get_balance('KRW') > 1000:
-     buy_money = upbit.get_balance('KRW') / len(buy_list) * 0.9994
-     for item in buy_list:
-       upbit.buy_market_order(item, buy_money)
-       earning_rate['현재 구매액'][item] += buy_money
-       earning_rate['총 구매액'][item] += buy_money
-       time.sleep(0.21)
+  if (upbit.get_balance('KRW') > 1000) and len(buy_list):
+    buy_money = upbit.get_balance('KRW') / len(buy_list) * 0.70
+    for item in buy_list:
+      upbit.buy_market_order(item, buy_money)
+      earning_rate['현재 구매액'][item] += buy_money
+      earning_rate['총 구매액'][item] += buy_money
+      time.sleep(0.21)
+    save_earning_rate(earning_rate)
--- AI study/coin/upbit/pyupbit-autotrade-main/test.py
+++ AI study/coin/upbit/pyupbit-autotrade-main/test.py
@@ -1,21 +1,144 @@
+# pyupbit install
+# !pip install pyupbit
+
+# 필요 라이브러리 import
 import time
 import pyupbit
 import datetime
 import requests
 import numpy as np
+import pandas as pd
 
+# 딥러닝을 구동하는 데 필요한 케라스 함수를 불러옵니다.
+import keras
+import tensorflow.keras.layers as Layer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, BatchNormalization, Conv2D, MaxPooling2D, Flatten
+
+# 케라스 외의 필요한 라이브러리를 불러옵니다.
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+# Ture가 나와야 GPU가 작동 되는 중 !
+from tensorflow.python.client import device_lib 
+device_lib.list_local_devices() 
+tf.test.is_gpu_available()
+
+## 개인정보 관리 주의 -----------------------------------------------##
 access = "naahaVSh29BJPu3pfBZPEWSyjsGwytkYiexmAuHR"
 secret = "e0fJfTu1BjPnn35Qc2pAOheSn7xCOXIMxG0Dzluj"
 myToken = "xoxb-1995417427669-2004663172180-7lt6OQoJLd2U5hbnMg8pirL9"
-slack_channel = "#trade"
-coin_name = "KRW-BTC"
-
+##-------------------------------------------------------------------##
 
 upbit = pyupbit.Upbit(access, secret)
+tickers = pyupbit.get_tickers('KRW')
 
-print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
+# 모델 학습 함수
+def model_train(train_size = 100, interval_time = 'minute60', data_limit = 3000):
+  tickers_count = 0
 
-tickers = pyupbit.get_tickers()
+  for item in tickers:
+    tickers_count += 1
+    df = pyupbit.get_ohlcv(item, interval=interval_time, count=data_limit)
+    # orderbook = pyupbit.get_orderbook(item)
+    
+    X = np.array([np.array(df.close.iloc[a:a+train_size]) for a in range(len(df) - train_size)])
+    Y = np.array(df.close.iloc[train_size:])
 
-print(tickers)
+    # 훈련, 테스트 셋 분리
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.80, test_size=0.20, random_state=2)
+
+    ## model
+    # 딥러닝 구조를 결정합니다(모델을 설정하고 실행하는 부분입니다).
+    globals()['{}_model'.format(item)] = Sequential([
+        BatchNormalization(),
+        Dense(128, activation='relu'),
+        Layer.Dropout(0.2),
+        Dense(256, activation='relu'),
+        Layer.Dropout(0.2),
+        Dense(512, activation='relu'),
+        Dense(1, activation='relu') # 분류할 방법에 따라 개수를 조정해야 합니다. 
+    ])
+
+    # overfitting을 방지하기 위해서 학습 중 early stop을 수행하기 위한 코드입니다.
+    early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
+
+    # 딥러닝을 실행합니다.
+    globals()['{}_model'.format(item)].compile(loss='mean_squared_error', # mean_squared_error # binary_crossentropy # mean_absolute_error # poisson
+                  optimizer='adam', 
+                  metrics=['accuracy']) 
+
+    print('진행도 :', tickers_count, '/', len(tickers), '| item :', item, '')
+    globals()['{}_model'.format(item)].fit(X_train, y_train, validation_data=(X_test,y_test), epochs=100, batch_size=train_size, verbose=0, callbacks=[early_stop])
+    
+  
+# 구매할 코인 리스트 작성 함수
+def buy_coin_list(train_size = 100, interval_time = 'minute60', data_limit = 100):
+  percent_list = dict()
+  tickers_count = 0
+
+  for item in tickers:
+    tickers_count += 1
+    df = pyupbit.get_ohlcv(item, interval=interval_time, count=data_limit)
+    pred_X = np.array([df.close.iloc[-train_size:]])
+    pred = globals()['{}_model'.format(item)].predict(pred_X)
+    percent_list[item] = ((pred[0][0] - df.close.iloc[-1]) / df.close.iloc[-1]) * 100
+    print('진행도 :', tickers_count, '/', len(tickers), '| item :', item, '| Price :', df.close.iloc[-1], '| Pred :', pred[0][0], '| Percent :', round(percent_list[item]), '%')
+
+  tmp = sorted(percent_list.items(), key=lambda x: x[1], reverse=True)
+  print(tmp)
+
+  buy_list = []
+  for l in tmp:
+    if 0 > l[1] > -0.7:
+      # print(l)
+      buy_list.append(l)
+
+  return buy_list
+
+earning_rate = pd.DataFrame(columns=['start', 'earning_rate'])
+# 시작 금액 측정
+# 코인 판매.
+for item in tickers:
+    coin = upbit.get_balance(item)
+    if coin:
+        upbit.sell_market_order(item, coin)
+        time.sleep(0.21)
+tmp = dict()
+tmp['start'] = upbit.get_balance("KRW")
+
+while True:
+  try:
+    model_train()
+    print(earning_rate)
+    time.sleep(3601 - datetime.datetime.now().minute * 60 - datetime.datetime.now().second)
+
+    # 코인 판매.
+    for item in tickers:
+      coin = upbit.get_balance(item)
+      if coin:
+        upbit.sell_market_order(item, coin)
+        time.sleep(0.21)
+
+    # 수익률 갱신
+    tmp['earning_rate'] = round((upbit.get_balance('KRW') - tmp['start']) / tmp['start'] , 4) * 100
+    earning_rate.loc[datetime.datetime.now().hour] = tmp
+    
+    # 수익률 csv 파일로 저장
+    earning_rate.to_csv('./earning_rate.csv')
+    
+    # 시작 금액 갱신
+    tmp['start'] = upbit.get_balance('KRW')
+
+    buy_list = buy_coin_list()
+    print(buy_list)
+
+    # 코인 구매
+    n = len(buy_list)
+    buy_money = round(upbit.get_balance("KRW") / n) * 0.998
+    for item in buy_list:
+      upbit.buy_market_order(item[0], buy_money)
+      time.sleep(0.21)
+  except Exception as e:
+    print(e)
+    time.sleep(1)
