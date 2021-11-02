0ca1fa01483b42b5d95703521f544b08cc06ca2e
KyungRyeol <foreversanta@naver.com>
2021-11-02 09:18:59 +0900

    morphing

--- AI study/coin/upbit/pyupbit-autotrade-main/auto_trade.py
+++ AI study/coin/upbit/pyupbit-autotrade-main/auto_trade.py
@@ -39,64 +39,74 @@ tickers = pyupbit.get_tickers('KRW')
 # 매도량이 매수량보다 많을 시 판매
 
 # 코인 판매.
-# for item in tickers:
-#     coin = upbit.get_balance(item)
-#     if coin:
-#         upbit.sell_market_order(item, coin)
-#         time.sleep(0.21)
-#     time.sleep(0.03)
+for item in tickers:
+    coin = upbit.get_balance(item)
+    if coin:
+        upbit.sell_market_order(item, coin)
+        time.sleep(0.21)
+    time.sleep(0.03)
 
 # test = tickers[0]
 # print(test)
-# time.sleep(61 - datetime.datetime.now().second)
-# print(pyupbit.get_ohlcv(test, interval='minute1', count=3))
+# # time.sleep(61 - datetime.datetime.now().second)
+# print(pyupbit.get_ohlcv(test, interval='minute1', count=2)['close'])
 # print(pyupbit.get_orderbook(test)['total_ask_size'])
 # print(pyupbit.get_orderbook(test)['total_bid_size'])
 # print(pyupbit.get_orderbook(test))
 
 # 수익률 저장
 def save_earning_rate(df):
-    earning_rate = df.sort_values(by = '수익률', axis = 0, ascending=False)
+    earning_rate = df.sort_values(by = '수익', axis = 0, ascending=False)
     print(earning_rate)
     # 수익률 csv 파일로 저장
     earning_rate.to_csv('./earning_rate.csv')
 
 구매한코인가격 = dict()
-수익률 = pd.DataFrame(data = [[0, 0, 0, 0]]*len(tickers), columns=['구매가격', '수익', '총구매액', '수익률'], index=tickers)
+수익률 = pd.DataFrame(data = [[0, 0, 0, 0, 0, 0]]*len(tickers), columns=['시작금액', '현재금액', '구매가격', '수익', '총구매액', '수익률'], index=tickers)
+수익률['시작금액'] = round(upbit.get_balance('KRW'), 2)
 
 while True:
     print(구매한코인가격)
     save_earning_rate(수익률)
-    time.sleep(61 - datetime.datetime.now().second)
-
-    for item in tickers:
-        거래량 = pyupbit.get_ohlcv(item, interval='minute1', count=3)['volume']
-        if 거래량[0]*5 < 거래량[1]:
-            매도량 = pyupbit.get_orderbook(item)['total_ask_size']
-            매수량 = pyupbit.get_orderbook(item)['total_bid_size']
-            현재보유액 = upbit.get_balance('KRW')
-            if (매도량*5 < 매수량) and (현재보유액 > 8000):
-                구매할금액 = 현재보유액 * 0.70
-                upbit.buy_market_order(item, 구매할금액)
-                구매한가격 = pyupbit.get_current_price(item)
-                구매한코인가격[item] = 구매한가격
-                수익률['구매가격'][item] = 구매한가격
-                수익률['총구매액'][item] += 구매한가격
-                print('종목 :', item, '거래량 :', 거래량[0], 거래량[1], '매도량 :', 매도량, '매수량 :', 매수량)
-        coin = upbit.get_balance(item)
-        if coin:
-            매도량 = pyupbit.get_orderbook(item)['total_ask_size']
-            매수량 = pyupbit.get_orderbook(item)['total_bid_size']
-            현재가 = pyupbit.get_current_price(item)
-            if item in 구매한코인가격:
-                if 매도량 > 매수량 and 현재가 > (구매한코인가격[item]*1.0005):
-                    upbit.sell_market_order(item, coin)
-                    수익률['수익'][item] += 현재가 - 수익률['구매가격'][item]
-                    수익률['수익률'][item] = round(수익률['수익'][item] / 수익률['총구매액'][item] * 100, 2)
-                    수익률['구매가격'][item] = 0
-                    print('종목 :', item, '판매가 :', 현재가)
-                    del 구매한코인가격[item]
+    수익률['현재금액'] = round(upbit.get_balance('KRW'), 2)
+    while time.sleep(61 - datetime.datetime.now().second):
+        for item in tickers:
+            coin = upbit.get_balance(item)
+            if coin:
+                # 매도량 = pyupbit.get_orderbook(item)['total_ask_size']
+                # 매수량 = pyupbit.get_orderbook(item)['total_bid_size']
+                현재가 = pyupbit.get_current_price(item)
+                이전가 = pyupbit.get_ohlcv(item, interval='minute1', count=2)['close'][0]
+                if 현재가 < 이전가:
+                    if item in 구매한코인가격: 
+                        if 현재가 > (구매한코인가격[item]*1.001):
+                            upbit.sell_market_order(item, coin)
+                            수익률['수익'][item] += 현재가 - 수익률['구매가격'][item]
+                            수익률['수익률'][item] = round(수익률['수익'][item] / 수익률['총구매액'][item] * 100, 2)
+                            수익률['구매가격'][item] = 0
+                            print('종목 :', item, '판매가 :', 현재가)
+                            del 구매한코인가격[item]
         
+    for item in tickers:
+        if item not in 구매한코인가격:
+            try:
+                거래량 = pyupbit.get_ohlcv(item, interval='minute1', count=3)['volume']
+                if 거래량[0]*10 < 거래량[1]:
+                    매도량 = pyupbit.get_orderbook(item)['total_ask_size']
+                    매수량 = pyupbit.get_orderbook(item)['total_bid_size']
+                    현재보유액 = upbit.get_balance('KRW')
+                    if (매도량*3 < 매수량) and (현재보유액 > 8000):
+                        구매할금액 = 현재보유액 * 0.70
+                        upbit.buy_market_order(item, 구매할금액)
+                        구매한가격 = pyupbit.get_current_price(item)
+                        구매한코인가격[item] = 구매한가격
+                        수익률['구매가격'][item] = 구매한가격
+                        수익률['총구매액'][item] += 구매한가격
+                        print('종목 :', item, '거래량 :', 거래량[0], 거래량[1], '매도량 :', 매도량, '매수량 :', 매수량)
+            except:
+                pass
+        
+            
 
 
 
