4c62a6dc4638b37085f7cc5d34551e8625ccfe48
forev <foreversanta@naver.com>
2022-02-26 16:20:46 +0900

    lv2 update

--- programmers/Level 2/행렬 테두리 회전하기.py
+++ programmers/Level 2/행렬 테두리 회전하기.py
@@ -33,7 +33,39 @@ def solution(rows, columns, queries):
     
     return answer
 
+# 다른 풀이
+from collections import deque
+def solution(rows, columns, queries):
+    arr = [[i+columns*j for i in range(1,columns+1)] for j in range(rows)]
+    answer, result = deque(), []
+    for i in queries:
+        a,b,c,d = i[0]-1,i[1]-1,i[2]-1,i[3]-1
+        for x in range(d-b):
+            answer.append(arr[a][b+x])
+        for y in range(c-a):
+            answer.append(arr[a+y][d])
+        for z in range(d-b):
+            answer.append(arr[c][d-z])
+        for k in range(c-a):
+            answer.append(arr[c-k][b])
+        answer.rotate(1)
+        result.append(min(answer))
+        for x in range(d-b):
+            arr[a][b+x] = answer[0]
+            answer.popleft()
+        for y in range(c-a):
+            arr[a+y][d] = answer[0]
+            answer.popleft()
+        for z in range(d-b):
+            arr[c][d-z] = answer[0]
+            answer.popleft()
+        for k in range(c-a):
+            arr[c-k][b] = answer[0]
+            answer.popleft()
+    return result
 
+# 메모
+# deque 사용 pop 속도 빠름 : https://www.notion.so/Deque-pop-push-64372aac99ea4b4a83b5feb6f61f70ec
 
 # 행렬 테두리 회전하기
 # 문제 설명
