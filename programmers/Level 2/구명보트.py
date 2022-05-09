4404607d9b28939f62fae7b3b3fe2d2f78cbd598
forev <foreversanta@naver.com>
2022-05-09 14:15:44 +0900

    lv3 정수 삼각형

--- programmers/Level 2/구명보트.py
+++ programmers/Level 2/구명보트.py
@@ -32,6 +32,21 @@ def solution(people, limit):
 # enumerate : [https://www.notion.so/Python-enumerate-6ae496d132c74d499f66595399224b6d](https://www.notion.so/Python-enumerate-6ae496d132c74d499f66595399224b6d)
 
 
+### 다른 풀이   
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
+
+
 
 # 구명보트
 # 문제 설명
