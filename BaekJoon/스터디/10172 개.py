c84bad07cb077989ab1eae2e816b93421fdce7e4
MAZE-Ryeol <ryeol_7@a-maze.ai>
2023-12-20 23:50:58 +0900

    10172: 개

---
+++ BaekJoon/스터디/10172 개.py
@@ -0,0 +1,54 @@
+print('|\\_/|')
+print('|q p|   /}')
+print('( 0 )\"\"\"\\')
+print('|\"^\"`    |')
+print('||_/=\\\\__|')
+
+'''
+# 개 성공다국어
+
+| 시간 제한 | 메모리 제한 | 제출 | 정답 | 맞힌 사람 | 정답 비율 |
+| --- | --- | --- | --- | --- | --- |
+| 1 초 | 256 MB | 419654 | 173172 | 152098 | 42.184% |
+
+## 문제
+
+아래 예제와 같이 개를 출력하시오.
+
+## 입력
+
+없음.
+
+## 출력
+
+개를 출력한다.
+
+## 예제 입력 1
+
+```
+
+```
+
+## 예제 출력 1
+
+```
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+
+```
+
+## 출처
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2014](https://www.acmicpc.net/category/782) > [Novice](https://www.acmicpc.net/category/detail/1275) 3번
+
+## 알고리즘 분류
+
+- [구현](https://www.acmicpc.net/problem/tag/102)
+
+## 메모
+
+[\ 뒤에 문자가 있으면 특수 문자.](https://www.notion.so/b3f6f45e9f124b5eb6213218b3934068?pvs=21)
+'''
