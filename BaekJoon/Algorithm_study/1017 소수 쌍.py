7e5047e805752b22c20ff7f8578ca15af9cffc8b
forev <foreversanta@naver.com>
2024-01-13 23:55:39 +0900

    1546: 평균

--- BaekJoon/Algorithm_study/1017 소수 쌍.py
+++ BaekJoon/Algorithm_study/1017 소수 쌍.py
@@ -1,52 +1,44 @@
 import sys
 import math
+import itertools
 
 
 # 소수 판별 함수
-def is_prime_number(x):
+def is_prime(x):
     if x < 2:
-        return False        # 2 이하 소수 아님
-    # 2부터 x의 제곱근까지의 모든 수를 확인하며
+        return False
     for i in range(2, int(math.sqrt(x)) + 1):
-        # x가 해당 수로 나누어떨어진다면
         if x % i == 0:
-            return False    # 소수가 아님
-    return True             # 소수임
+            return False
+    return True
 
 
-def find_all_pairs(lst):
-    def backtrack(start, path):
-        if len(path) == len(lst) // 2:
-            result.append(path)
-            return
-        for i in range(start, len(lst)):
-            for j in range(i + 1, len(lst)):
-                if not used[i] and not used[j]:
-                    used[i] = True
-                    used[j] = True
-                    backtrack(i + 1, path + [(lst[i], lst[j])])
-                    used[i] = False
-                    used[j] = False
-            print(result)
-
-    result = []
-    used = [False] * len(lst)
-    backtrack(0, [])
-    return result
+# 리스트에서 두 요소의 합이 소수인 모든 순서쌍 찾기
+def find_prime_pairs(lst):
+    prime_pairs = []
+    for pair in itertools.combinations(lst, 2):
+        if is_prime(sum(pair)):
+            prime_pairs.append(pair)
+    return prime_pairs
 
 
-input = sys.stdin.readline
+# 소수 합 순서쌍을 사용하여 전체 조합 만들기
+def create_combinations(lst):
+    first_num = lst[0]
+    combinations, done_pairs = [], []
+    prime_pairs = find_prime_pairs(lst)
+    for comb in itertools.combinations(prime_pairs, len(lst)//2):
+        flat_comb = sum(comb, ())
+        if (flat_comb[0] == first_num) and (flat_comb[1] not in done_pairs) and len(set(flat_comb)) == N:
+            combinations.append(comb)
+            done_pairs.append(flat_comb[1])
+    return done_pairs
+
+
 N = int(input().strip())
-_list = list(map(int, input().strip().split()))
-answer = set([])
-for pair in find_all_pairs(_list):
-    for p in pair:
-        if not is_prime_number(sum(p)):
-            break
-    else:
-        answer.add(pair[0][1])
-    print(pair)
-if answer:
-    print(' '.join(map(str, sorted(answer))))
+lst = list(map(int, input().strip().split()))
+result = sorted(create_combinations(lst))
+if result:
+    print(' '.join(map(str, result)))
 else:
     print(-1)
