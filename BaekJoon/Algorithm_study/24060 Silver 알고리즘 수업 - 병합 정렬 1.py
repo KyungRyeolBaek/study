b345aa82ada16aacd70830d5f8c8b9478393f5ab
forev <foreversanta@naver.com>
2024-07-15 00:26:05 +0900

    24060: 알고리즘 수업 - 병합 정렬 1

--- BaekJoon/Algorithm_study/24060 Silver 알고리즘 수업 - 병합 정렬 1.py
+++ BaekJoon/Algorithm_study/24060 Silver 알고리즘 수업 - 병합 정렬 1.py
@@ -30,55 +30,54 @@ import sys
 
 
 class Merge_Sort:
-    def __init__(self, k):
+    def __init__(self, K):
         self.count = 0
-        self.k = k
+        self.k = K
         self.result = 0
 
-    def merge_sort(self, unsorted_list):
-        # 크기가 1이하면 반환
-        if len(unsorted_list) <= 1:
-            return unsorted_list
-        
-        # 리스트를 2분할
-        mid = len(unsorted_list) // 2
-        left = unsorted_list[:mid]
-        right = unsorted_list[mid:]
-        
-        # 2분할한 리스트를 각각 merge sort진행
-        left_ = self.merge_sort(left)
-        right_ = self.merge_sort(right)
-        return self.merge(left_, right_)
+    def merge_sort(self, A, p, r):
+        # A[p..r]을 오름차순 정렬한다.
+        if p < r:
+            q = (p + r) // 2  # q는 p, r의 중간 지점
+            self.merge_sort(A, p, q)  # 전반부 정렬
+            self.merge_sort(A, q + 1, r)  # 후반부 정렬
+            self.merge(A, p, q, r)  # 병합
 
-    def merge(self, left, right):
-        i, j = 0,0
-        sorted_list = []
+    def merge(self, A, p, q, r):
+        # A[p..q]와 A[q+1..r]을 병합하여 A[p..r]을 오름차순 정렬된 상태로 만든다.
+        # A[p..q]와 A[q+1..r]은 이미 오름차순으로 정렬되어 있다.
+        i = p
+        j = q + 1
+        t = 0
+        tmp = [0] * (r - p + 1)
         
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                sorted_list.append(left[i])
+        while i <= q and j <= r:
+            if A[i] <= A[j]:
+                tmp[t] = A[i]
                 i += 1
             else:
-                sorted_list.append(right[j])
+                tmp[t] = A[j]
                 j += 1
-            self.save_result(sorted_list)
-
-        while i < len(left):
-            sorted_list.append(left[i])
+            t += 1
+        
+        while i <= q:  # 왼쪽 배열 부분이 남은 경우
+            tmp[t] = A[i]
             i += 1
-            self.save_result(sorted_list)
-
-        while j < len(right):
-            sorted_list.append(right[j])
+            t += 1
+        
+        while j <= r:  # 오른쪽 배열 부분이 남은 경우
+            tmp[t] = A[j]
             j += 1
-            self.save_result(sorted_list)
-            
-        return sorted_list
-    
-    def save_result(self, sorted_list):
+            t += 1
+        
+        for i in range(p, r + 1):
+            A[i] = tmp[i - p]
+            self.save_result(tmp[i - p])
+
+    def save_result(self, result):
         self.count += 1
         if self.count == self.k:
-            self.result = sorted_list[-1]
+            self.result = result
 
 
 input = sys.stdin.readline
@@ -86,7 +85,7 @@ size, K = map(int, input().strip().split())
 A = list(map(int, input().strip().split()))
 
 Merge_Sort_ = Merge_Sort(K)
-Merge_Sort_.merge_sort(A)
+Merge_Sort_.merge_sort(A, 0, len(A) - 1)
 if Merge_Sort_.result != 0:
     print(Merge_Sort_.result)
 else:
