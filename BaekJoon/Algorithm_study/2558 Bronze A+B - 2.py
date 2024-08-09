dea4b78bfea9e46f32667815ee628052da985063
forev <foreversanta@naver.com>
2024-08-10 00:21:48 +0900

    2558: A+B - 2

---
+++ BaekJoon/Algorithm_study/2558 Bronze A+B - 2.py
@@ -0,0 +1,73 @@
+import sys
+
+
+input = sys.stdin.readline
+A = int(input())
+B = int(input())
+print(A + B)
+
+'''
+# A+B - 2
+
+| 시간 제한 | 메모리 제한 | 제출 | 정답 | 맞힌 사람 | 정답 비율 |
+| --- | --- | --- | --- | --- | --- |
+| 1 초 | 128 MB | 114349 | 81055 | 72922 | 72.376% |
+
+## 문제
+
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+## 입력
+
+첫째 줄에 A, 둘째 줄에 B가 주어진다. (0 < A, B < 10)
+
+## 출력
+
+첫째 줄에 A+B를 출력한다.
+
+## 예제 입력 1
+
+```
+1
+2
+
+```
+
+## 예제 출력 1
+
+```
+3
+
+```
+
+## 출처
+
+- 빠진 조건을 찾은 사람: [djm03178](https://www.acmicpc.net/user/djm03178)
+
+## 비슷한 문제
+
+- [1000번. A+B](https://www.acmicpc.net/problem/1000)
+- [1001번. A-B](https://www.acmicpc.net/problem/1001)
+- [1008번. A/B](https://www.acmicpc.net/problem/1008)
+- [10950번. A+B - 3](https://www.acmicpc.net/problem/10950)
+- [10951번. A+B - 4](https://www.acmicpc.net/problem/10951)
+- [10952번. A+B - 5](https://www.acmicpc.net/problem/10952)
+- [10953번. A+B - 6](https://www.acmicpc.net/problem/10953)
+- [10998번. A×B](https://www.acmicpc.net/problem/10998)
+- [11021번. A+B - 7](https://www.acmicpc.net/problem/11021)
+- [11022번. A+B - 8](https://www.acmicpc.net/problem/11022)
+- [15740번. A+B - 9](https://www.acmicpc.net/problem/15740)
+- [15792번. A/B - 2](https://www.acmicpc.net/problem/15792)
+
+## 알고리즘 분류
+
+- [수학](https://www.acmicpc.net/problem/tag/124)
+- [구현](https://www.acmicpc.net/problem/tag/102)
+- [사칙연산](https://www.acmicpc.net/problem/tag/121)
+
+## 채점 및 기타 정보
+
+- 이 문제의 채점 우선 순위는 2이다.
+
+## 메모
+'''
