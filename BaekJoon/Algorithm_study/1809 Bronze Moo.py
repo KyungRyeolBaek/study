a5b8d34fd9e8c850e1dbce333e9c474d2a8e93bd
MAZE-Ryeol <ryeol_7@a-maze.ai>
2024-08-31 11:11:46 +0900

    1809: Moo

---
+++ BaekJoon/Algorithm_study/1809 Bronze Moo.py
@@ -0,0 +1,61 @@
+import sys
+
+
+input = sys.stdin.readline
+print("(___)")
+print("(o o)____/")
+print(" @@      \\")
+print("  \ ____,/")
+print("  //   //")
+print(" ^^   ^^")
+
+'''
+# Moo 다국어언어 제한
+
+| 시간 제한 | 메모리 제한 | 제출 | 정답 | 맞힌 사람 | 정답 비율 |
+| --- | --- | --- | --- | --- | --- |
+| 2 초 | 128 MB | 11241 | 4624 | 4442 | 48.520% |
+
+## 문제
+
+You’ve decided to buy a farm and start a new life. To pass some time while you wait for the title of the land to go through, produce the following ascii cow.
+
+## 예제 입력 1 복사
+
+```
+
+```
+
+## 예제 출력 1 복사
+
+```
+(___)
+(o o)____/
+ @@      \
+  \ ____,/
+  //   //
+ ^^   ^^
+
+```
+
+## 노트
+
+실제 대회에는 언어 제한이 없었다.
+
+## 출처
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2017](https://www.acmicpc.net/category/784) > [Novice](https://www.acmicpc.net/category/detail/3298) 1번
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2017](https://www.acmicpc.net/category/784) > [Advanced](https://www.acmicpc.net/category/detail/3299) 1번
+
+## 알고리즘 분류
+
+[보기](https://www.acmicpc.net/problem/1809#)
+
+## 제출할 수 있는 언어
+
+Golfscript
+
+## 메모
+'''
+
