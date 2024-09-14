ea4c9cd4ddd899a4639e0e542ed2177a5ea2d105
forev <foreversanta@naver.com>
2024-09-14 22:46:47 +0900

    5337 Bronze 웰컴.py

---
+++ BaekJoon/Algorithm_study/5337 Bronze 웰컴.py
@@ -0,0 +1,46 @@
+print('.  .   .')
+print('|  | _ | _. _ ._ _  _')
+print('|/\\|(/.|(_.(_)[ | )(/.')
+
+'''
+# 웰컴 다국어한국어
+
+| 시간 제한 | 메모리 제한 | 제출 | 정답 | 맞힌 사람 | 정답 비율 |
+| --- | --- | --- | --- | --- | --- |
+| 1 초 | 128 MB | 26147 | 18457 | 17705 | 72.588% |
+
+## 문제
+
+Welcome을 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+## 출력
+
+Welcome을 아래 예제 출력처럼 출력한다.
+
+## 예제 입력 1 복사
+
+```
+
+```
+
+## 예제 출력 1 복사
+
+```
+.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+
+```
+
+## 출처
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2013](https://www.acmicpc.net/category/781) > [Novice](https://www.acmicpc.net/category/detail/464) 1번
+
+- 문제를 번역한 사람: [baekjoon](https://www.acmicpc.net/user/baekjoon)
+
+## 알고리즘 분류
+
+- [구현](https://www.acmicpc.net/problem/tag/102)
+
+## 메모
+'''
