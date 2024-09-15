6387ce272e5eda484fce7e24feecbc340c263d24
forev <foreversanta@naver.com>
2024-09-15 19:31:44 +0900

    5338 마이크로 소프트 로고

---
+++ BaekJoon/Algorithm_study/5338 Bronze 마이크로소프트 로고.py
@@ -0,0 +1,51 @@
+print('''       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|'''
+)
+
+''''
+# 마이크로소프트 로고 다국어한국어
+
+| 시간 제한 | 메모리 제한 | 제출 | 정답 | 맞힌 사람 | 정답 비율 |
+| --- | --- | --- | --- | --- | --- |
+| 1 초 | 128 MB | 26998 | 21331 | 20079 | 80.162% |
+
+## 문제
+
+마이크로소프트 로고를 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+## 출력
+
+마이크로소프트 로고를 예제 출력처럼 출력한다.
+
+## 예제 입력 1 복사
+
+```
+
+```
+
+## 예제 출력 1 복사
+
+```
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+
+```
+
+## 출처
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2013](https://www.acmicpc.net/category/781) > [Novice](https://www.acmicpc.net/category/detail/464) 2번
+
+- 문제를 번역한 사람: [baekjoon](https://www.acmicpc.net/user/baekjoon)
+
+## 알고리즘 분류
+
+- [구현](https://www.acmicpc.net/problem/tag/102)
+
+## 메모
+'''
