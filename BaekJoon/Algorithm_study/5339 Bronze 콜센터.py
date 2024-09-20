7ef84bfb75baabc6c5e5f70b144e3a272ef64a03
forev <foreversanta@naver.com>
2024-09-20 23:01:06 +0900

    5339: 콜센터

---
+++ BaekJoon/Algorithm_study/5339 Bronze 콜센터.py
@@ -0,0 +1,64 @@
+print('''     /~\\
+    ( oo|
+    _\\=/_
+   /  _  \\
+  //|/.\\|\\\\
+ ||  \\ /  ||
+============
+|          |
+|          |
+|          |''')
+
+'''
+# 콜센터 다국어한국어
+
+| 시간 제한 | 메모리 제한 | 제출 | 정답 | 맞힌 사람 | 정답 비율 |
+| --- | --- | --- | --- | --- | --- |
+| 1 초 | 128 MB | 24033 | 15860 | 15089 | 67.618% |
+
+## 문제
+
+스타워즈에 등장하는 로봇인 C3PO는 요즘 콜센터에 근무하고 있다. 콜센터에 앉아있는 C3PO를 그리는 프로그램을 작성하시오.
+
+## 출력
+
+예제 출력처럼 콜센터에 앉아있는 C3PO를 출력한다. 마지막 세 줄의 두 '|' 사이에는 공백이 10개 있다.
+
+## 예제 입력 1 복사
+
+```
+
+```
+
+## 예제 출력 1 복사
+
+```
+     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |
+
+```
+
+## 출처
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2013](https://www.acmicpc.net/category/781) > [Advanced](https://www.acmicpc.net/category/detail/463) 1번
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2013](https://www.acmicpc.net/category/781) > [Novice](https://www.acmicpc.net/category/detail/464) 3번
+
+- 문제를 번역한 사람: [baekjoon](https://www.acmicpc.net/user/baekjoon)
+- 잘못된 데이터를 찾은 사람: [sait2000](https://www.acmicpc.net/user/sait2000)
+- 문제의 오타를 찾은 사람: [sky0331](https://www.acmicpc.net/user/sky0331)
+
+## 알고리즘 분류
+
+- [구현](https://www.acmicpc.net/problem/tag/102)
+
+## 메모
+'''
