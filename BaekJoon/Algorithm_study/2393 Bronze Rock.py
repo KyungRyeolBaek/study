52dee315481f39f15d54595d8c5d14ee2a733559
forev <foreversanta@naver.com>
2024-09-21 21:10:03 +0900

    2393: Rock

---
+++ BaekJoon/Algorithm_study/2393 Bronze Rock.py
@@ -0,0 +1,67 @@
+print('''  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \\_________/
+    \\_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \\__
+ /             \\
+/_______________\\''')
+
+'''
+# Rook 다국어
+
+| 시간 제한 | 메모리 제한 | 제출 | 정답 | 맞힌 사람 | 정답 비율 |
+| --- | --- | --- | --- | --- | --- |
+| 1 초 | 128 MB | 9646 | 5433 | 5139 | 58.994% |
+
+## 문제
+
+You have just learned how to output text to the screen and your teacher has challenged you to create an ASCII art of a chess piece. You have decided to make your favorite piece, the rook.
+
+## 출력
+
+The rook art, exactly as shown below, with no extra blank spaces. In particular, a line must not end with a blank space.
+
+## 예제 입력 1 복사
+
+```
+
+```
+
+## 예제 출력 1 복사
+
+```
+  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \
+/_______________\
+
+```
+
+## 출처
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2020](https://www.acmicpc.net/category/787) > [Novice](https://www.acmicpc.net/category/detail/3304) 10번
+
+[High School](https://www.acmicpc.net/category/97) > [PLU High School Programming Contest](https://www.acmicpc.net/category/96) > [PLU 2020](https://www.acmicpc.net/category/787) > [Advanced](https://www.acmicpc.net/category/detail/3305) 8번
+
+## 알고리즘 분류
+
+- [구현](https://www.acmicpc.net/problem/tag/102)
+
+## 메모
+'''
+
